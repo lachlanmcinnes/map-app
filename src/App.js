7d8017ce704860ee793f3e9ff3473832b28c22e3
Lachlan McInnes <lrmcinne@deakin.edu.au>
2019-09-18 11:10:31 +1000

    UPDATE: add autocomplete functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Map from 'pigeon-maps';
 import Marker from 'pigeon-marker'
+//import AlgoliaPlaces from 'algolia-places-react'
 import './App.css';
 
 const fetch=require("node-fetch");
 
+var places = require('places.js');
+
+
+
+
 class App extends React.Component{
+	
 	state = {
 		status : "ALL",
 		classification : "ALL",
-		events: []
+		events: [],
+		lat: "0",
+		lng: "0"
 	}
 	
 	statusChanged = (e) => {
@@ -27,7 +36,10 @@ class App extends React.Component{
 			url+=`classification=${this.state.classification}&`
 		}
 		if(this.state.status!=='ALL'){
-			url+=`status=${this.state.status}`
+			url+=`status=${this.state.status}&`
+		}
+		if(this.state.lat!=='0'){
+			url+=`$where=within_circle(the_geom,${this.state.lat},${this.state.lng},1000)`
 		}
 		
 		fetch(url)
@@ -42,6 +54,19 @@ class App extends React.Component{
 	
 	componentDidMount(){
 		this.fetchInfo();
+		
+		var placesAutoComplete = places({
+			appId: 'plY0BYIMLL9G',
+			apiKey: 'f14232e9f94d077e60d8007c1b05c5bb',
+			container: document.querySelector('#address'),
+			type: 'address'
+		});
+		
+		placesAutoComplete.on('change', e => {
+			this.setState({lat: e.suggestion.latlng.lat ,lng: e.suggestion.latlng.lng}, this.fetchInfo);
+			//this.setState({lng: e.suggestion.latlng.lng}), this.fetchInfo;
+			console.log(JSON.stringify(e.suggestion.latlng.lat))
+		})
 	}
 	
 	handleMarkerClick=({ event, payload, anchor }) => {
@@ -54,8 +79,11 @@ class App extends React.Component{
 		const classification = ['ALL', 'Public Event', 'Structures', 'Event', ]
 		return (
 			<div className="App">
+				<input type="search" id="address" class="form-control" placeholder="Where are we going?" />
+				
+				
 				<header className="App-header">
-					<Map center={[-37.8470585,145.1145445]} zoom={12} width={600} height={400}>
+					<Map center={[-37.8470585,145.1145445]} zoom={12} width={600} height={400} metaWheelZoom={'TRUE'}>
 						<Marker anchor={[-37.8470585,145.1145445]} payload={1}/>
 						{this.state.events.map(i=> (<Marker anchor={[i.the_geom.coordinates[0][0][0][1],i.the_geom.coordinates[0][0][0][0]]} payload={i} onClick={this.handleMarkerClick}/>))}
 					</Map>
